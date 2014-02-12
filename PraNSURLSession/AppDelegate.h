6d83e915462f07d904392cdf3c9ebc98a0d39d41
daisuke <banana0928@daisuke-MacBookAir.local>
2014-02-13 02:53:15 +0900

    Initial Commit

---
+++ PraNSURLSession/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PraNSURLSession
+//
+//  Created by daisuke on 2014/02/13.
+//  Copyright (c) 2014年 daisuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PraNSURLSession/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PraNSURLSession
+//
+//  Created by daisuke on 2014/02/13.
+//  Copyright (c) 2014年 daisuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
